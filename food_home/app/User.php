7e1e12dc1b47b3474acd7ee2fd3b5260bb167cba
bryan <2172526@my.ipleiria.pt>
2020-11-14 02:03:56 +0000

    Vue com a app e os componentes sidebar e topbar

--- food_home/app/User.php
+++ food_home/app/User.php
@@ -1,15 +1,14 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,9 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'last_name', 'email', 'password',
     ];
 
     /**
@@ -28,8 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
     /**
@@ -40,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getFullNameAttribute()
+    {
+        if (is_null($this->last_name)) {
+            return "{$this->name}";
+        }
+
+        return "{$this->name} {$this->last_name}";
+    }
 }
--- food_home/resources/js/app.js
+++ food_home/resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+window.Vue = require('vue')
+
+import AppComponent from './App.vue'
+
+Vue.component('app', AppComponent)
+
+const app = new Vue({
+    el: '#app'
+})
